a91e12260ac47ad3d0eafdac25100a4c9eab54d5
Reggie Murphy <reggiemurphy7@gmail.com>
2017-10-24 15:23:46 +0100

    Setup project structure with classes, layouts, manifest etc.

---
+++ AhHearApp/app/src/main/java/com/example/ahhear/ahhearapp/BandVenueHeatmap.java
@@ -0,0 +1,12 @@
+package com.example.ahhear.ahhearapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BandVenueHeatmap extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.heatmap);
+    }
+}
