5d4eba3f726006c45706958cc3393a142a92e784
hinfeyg2 <hinfeyg2@gmail.com>
2017-11-28 23:00:17 +0000

    gig id working with picklocation

--- AhHearApp/app/src/main/java/com/example/ahhear/ahhearapp/PickLocation.java
+++ AhHearApp/app/src/main/java/com/example/ahhear/ahhearapp/PickLocation.java
@@ -1,30 +1,52 @@
 package com.example.ahhear.ahhearapp;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.ArrayMap;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import javax.net.ssl.HttpsURLConnection;
+
+import ca.hss.heatmaplib.HeatMap;
 
 public class PickLocation extends AppCompatActivity {
     public static final String EXTRA_DECIBEL = "com.example.ahhear.MESSAGE";
@@ -50,15 +72,33 @@ public class PickLocation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+//        int gig_id = getIntent().getIntExtra("gigId", 0);
+        int gig_id = 10;
         setContentView(R.layout.pick_location);
 //        set dynamic image // uses picasso for better memory management
+
+        PickLocation.DownloadGigData downloadGigData = new PickLocation.DownloadGigData(this);
+        try {
+            // this is the url to the API.
+            // Static at the moment but its easy to add which gig were looking at.
+            Uri.Builder builder = new Uri.Builder();
+            builder.scheme("http");
+
+            builder.encodedAuthority("10.0.2.2:8000");
+            builder.appendPath("single_gig");
+
+            builder.appendQueryParameter("gig_id", String.valueOf(gig_id));
+            builder.build();
+            downloadGigData.execute(new URL(builder.toString()));
+
+        } catch (MalformedURLException e) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Error occurred", Toast.LENGTH_SHORT);
+            toast.show();
+            e.printStackTrace();
+        }
+
         ImageView imageView = (ImageView) findViewById(R.id.floorplan);
 
-        Picasso.with(this)
-                .load("https://s8.postimg.org/iqy8i4nsl/transparent_floorplan.png")
-                .into(imageView);
-
-        
         mRecordButton  = findViewById(R.id.button_rec);
         mRecordLabel = findViewById(R.id.textView_record_label);
         mDecibels = findViewById(R.id.textView_sound);
@@ -204,5 +244,99 @@ public class PickLocation extends AppCompatActivity {
         }
     };
 
+    private class DownloadGigData extends AsyncTask<URL, Integer, JSONArray> {
 
+        // record this activity as a variable for later.
+        // used to get the width of the screen for the heatmap.
+        private Activity activity;
+
+        private DownloadGigData(Activity activity) {
+            this.activity = activity;
+        }
+
+        // downloading the data has to be done asynchronous.
+        // this is robbed from charlotte's venue menu.
+        protected JSONArray doInBackground(URL... urls) {
+
+            JSONArray arr = null;
+
+            for (URL url : urls) {
+                try {
+                    URLConnection urlConnection = url.openConnection();
+                    urlConnection.setConnectTimeout(1000);
+                    BufferedInputStream is = new BufferedInputStream(urlConnection.getInputStream());
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                    StringBuilder sb = new StringBuilder();
+
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line).append('\n');
+                    }
+
+                    try {
+
+                        // the data comes down as a string
+                        // is parsed as json and added to a 2darray of doubles.
+
+                        arr = new JSONArray(sb.toString());
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // Escape early if cancel() is called
+                if (isCancelled()) break;
+            }
+
+            return arr;
+        }
+
+        // when the asynchronous download is finished this runs.
+        // displays the heatmap.
+        protected void onPostExecute(JSONArray ReturnArray) {
+
+            try {
+
+                JSONObject gigJSON = ReturnArray.getJSONObject(0);
+                String VenueId = gigJSON.getString("venue_id");
+                System.out.println(VenueId);
+
+                Uri.Builder builder = new Uri.Builder();
+                builder.scheme("http");
+                builder.encodedAuthority("gavs.work:8000");
+                builder.appendPath("floorplan_image");
+                builder.appendQueryParameter("id", VenueId);
+                builder.build();
+
+                ImageView imageView = (ImageView) findViewById(R.id.floorplan);
+
+                Picasso.with(activity)
+                        .load(builder.toString())
+                        .into(imageView);
+
+                String BandName = gigJSON.getString("band_name");
+                TextView bandView = (TextView)findViewById(R.id.LoactionBandName);
+                bandView.setText(BandName);
+
+                String VenueName = gigJSON.getString("venue_name");
+                TextView venueView=(TextView)findViewById(R.id.LoactionVenueName);
+                venueView.setText(VenueName);
+                System.out.println(VenueName);
+
+                String GigDate = gigJSON.getString("gig_date");
+                TextView DateView =(TextView)findViewById(R.id.LoactionGigDate);
+                DateView.setText(GigDate);
+
+            } catch (JSONException e) {
+                System.out.println("Json error getting band_id");
+            }
+
+        }
+
+    }
 }
