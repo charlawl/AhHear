4325424878ae5ef6e02d9e03ac08269836063d5a
hinfeyg2 <hinfeyg2@gmail.com>
2017-12-01 15:06:05 +0000

    comments for picklocation

--- AhHearApp/app/src/main/java/com/example/ahhear/ahhearapp/PickLocation.java
+++ AhHearApp/app/src/main/java/com/example/ahhear/ahhearapp/PickLocation.java
@@ -11,7 +11,6 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
